1d80b5a645213bae007801e0c7072958b3fcf563
gustavoAlv3ss <luckbr6666@gmail.com>
2023-07-24 19:22:03 -0300

    finalizado

---
+++ js/login.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const loginContainer = document.getElementById('login-content')
+
+const moveOverlay = () => loginContainer.classList.toggle('move')
+
+document.getElementById('open-register').addEventListener('click', moveOverlay)
+document.getElementById('open-login').addEventListener('click', moveOverlay)
