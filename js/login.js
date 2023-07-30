098fb822462d699721a5a355ffdac6b45d7b0da3
gustavoAlv3ss <luckbr6666@gmail.com>
2023-07-30 03:29:47 -0300

    mobile finalizado

--- js/login.js
+++ js/login.js
@@ -6,3 +6,6 @@ const moveOverlay = () => loginContainer.classList.toggle('move')
 
 document.getElementById('open-register').addEventListener('click', moveOverlay)
 document.getElementById('open-login').addEventListener('click', moveOverlay)
+
+document.getElementById('register-mobile').addEventListener('click', moveOverlay)
+document.getElementById('login-mobile').addEventListener('click', moveOverlay)
